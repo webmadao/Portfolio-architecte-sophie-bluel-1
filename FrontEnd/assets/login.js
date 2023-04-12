9680b128c1b5586f203e6944b807ba40bf808a01
Ilyes Kadji <web.kin.dev@gmail.com>
2023-04-12 03:43:33 +0200

    fonction handlelogin

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,50 +1,16 @@
 
-/*function handleLogin(event) {
-    event.preventDefault(); // Empêche le formulaire de recharger la page
-
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const myForm = document.getElementById('myForm');
-
-    fetch('http://localhost:5678/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Les informations d\'identification sont incorrectes.');
-            }
-            return response.json();
-        })
-        .then(data => {
-            window.location.href = 'index.html'; // Redirige vers la page d'accueil
-        })
-        .catch(error => {
-            const errorSpan = document.getElementById('error');
-            errorSpan.innerText = error.message;
-        });
-}
-
-// Ajouter un événement d'écouteur pour le bouton de connexion
-const loginButton = document.querySelector('#login-button');
-
-loginButton.addEventListener('click', handleLogin);*/
-
-/*
 function handleLogin(event) {
     event.preventDefault(); // Empêche le formulaire de recharger la page
 
+    // Récupération des valeurs des champs email et password
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+
+    // Récupération du formulaire "myForm"
     const myForm = document.getElementById('myForm');
 
-    fetch('http://localhost:5678/api/login', {
+    // Envoi d'une requête POST à l'API "http://localhost:5678/api/login" avec les valeurs des champs email et password
+    fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -55,102 +21,25 @@ function handleLogin(event) {
         })
     })
         .then(response => {
+            // Vérification que la réponse est valide (code de statut HTTP 200 à 299)
             if (!response.ok) {
                 throw new Error('Les informations d\'identification sont incorrectes.');
             }
+            // Conversion de la réponse au format JSON
             return response.json();
         })
         .then(data => {
-            // Redirige vers la page d'accueil après une connexion réussie
+            // Redirection vers la page d'accueil si les informations d'identification sont correctes
             window.location.href = 'index.html';
         })
         .catch(error => {
-            // Affiche l'erreur dans un élément HTML dédié
-            const errorSpan = document.getElementById('error');
-            errorSpan.textContent = error.message;
+            // Affichage d'un message d'erreur si les informations d'identification sont incorrectes ou si une erreur inattendue s'est produite
+            /* const errorSpan = document.getElementById('error');
+             errorSpan.innerText = error.message;*/
+            alert("Les informations d\'identification sont incorrectes");
         });
 }
 
-// Ajouter un événement d'écouteur pour le bouton de connexion
-const loginButton = document.querySelector('login-button');
-if (loginButton) { // Vérifie si l'élément est présent dans la page
-    loginButton.addEventListener('click', handleLogin);
-}*/
-
-
-/*
-const loginForm = document.querySelector('#myForm');
-
-loginForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  const formData = new FormData(loginForm);
-  const email = formData.get('email');
-  const password = formData.get('password');
-
-  const response = await fetch('http://localhost:5678/api/login', {
-    method: 'POST',
-    body: JSON.stringify({ email, password }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-
-  const result = await response.json();
-
-  if (response.ok) {
-    // Rediriger vers la page d'accueil
-    window.location.href = 'index.html';
-  } else {
-    // Afficher un message d'erreur
-    const errorElement = document.querySelector('#error');
-    errorElement.textContent = result.message;
-  }
-});*/
-/*
-document.getElementById("myFrom").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert('Formulaire envoyé!')
-})*/
-
-/*
-const loginForm = document.querySelector('#login');
-
-loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(loginForm);
-    const username = formData.get('email');
-    const password = formData.get('password');
-
-    const response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    const result = await response.json();
-
-    if (response.ok) {
-        // Rediriger vers la page d'accueil
-        window.location.href = 'index.html';
-    } else {
-        // Afficher un message d'erreur
-        const errorElement = document.querySelector('#error-message');
-        errorElement.textContent = result.message;
-    }
-});*/
-
-async function getLogin(user) {
-    let response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(user)
-    });
-}
-getLogin()
-
+// Ajout d'un événement d'écouteur pour le bouton de connexion
+const loginButton = document.querySelector('#login-button');
+loginButton.addEventListener('click', handleLogin);
