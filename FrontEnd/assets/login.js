d3d270190cafc5d2c4c93e02a91e3c970441e9e4
Ilyes Kadji <web.kin.dev@gmail.com>
2023-05-05 10:11:22 +0200

    com

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,4 +1,70 @@
 "use strict";
+function handleLogin(event) {
+    event.preventDefault(); // Empêche le formulaire de recharger la page
+
+    // Récupération des valeurs des champs email et password
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Récupération du formulaire "myForm"
+    /*const myForm = document.getElementById('myForm');*/
+
+    // Envoi d'une requête POST à l'API "http://localhost:5678/api/login" avec les valeurs des champs email et password
+    fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            accept: "application/json",
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+        .then(response => {
+            // Vérification que la réponse est valide (code de statut HTTP 200 à 299)
+
+            if (response.status === 401) {
+                throw new Error("L'adresse e-mail ou le mot de passe est incorrect.");
+            }
+            if (response.status !== 200) {
+                throw new Error("Une erreur s'est produite lors de la connexion.");
+            }
+            // Extraction des données JSON de la réponse
+            return response.json();
+        })
+        .then(data => {
+            // Stockage du token et de l'userId dans le stockage local
+            localStorage.setItem("userId", data.userId);
+            localStorage.setItem("token", data.token);
+            // Redirection vers la page d'accueil si les informations d'identification sont correctes
+            window.location.href = 'index.html';
+            // Affichage du bouton de déconnexion et masquage du bouton de connexion
+        })
+        .catch(error => {
+            // Affichage d'un message d'erreur si les informations d'identification sont incorrectes ou si une erreur inattendue s'est produite
+            alert("Les informations d\'identification sont incorrectes");
+        });
+}
+
+
+// Ajout d'un événement d'écouteur pour le bouton de connexion
+const loginButton = document.querySelector('#login-button');
+loginButton.addEventListener('click', handleLogin);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 // Constantes
 const API_URL = "http://localhost:5678/api/users/login";
@@ -74,61 +140,3 @@ function handleLogin(event) {
 // Ajout d'un événement d'écouteur pour le bouton de connexion
 const loginButton = document.querySelector("#login-button");
 loginButton.addEventListener("click", handleLogin);*/
-
-
-
-
-
-
-function handleLogin(event) {
-    event.preventDefault(); // Empêche le formulaire de recharger la page
-
-    // Récupération des valeurs des champs email et password
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    // Récupération du formulaire "myForm"
-    /*const myForm = document.getElementById('myForm');*/
-
-    // Envoi d'une requête POST à l'API "http://localhost:5678/api/login" avec les valeurs des champs email et password
-    fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            accept: "application/json",
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    })
-        .then(response => {
-            // Vérification que la réponse est valide (code de statut HTTP 200 à 299)
-
-            if (response.status === 401) {
-                throw new Error("L'adresse e-mail ou le mot de passe est incorrect.");
-            }
-            if (response.status !== 200) {
-                throw new Error("Une erreur s'est produite lors de la connexion.");
-            }
-            // Extraction des données JSON de la réponse
-            return response.json();
-        })
-        .then(data => {
-            // Stockage du token et de l'userId dans le stockage local
-            localStorage.setItem("userId", data.userId);
-            localStorage.setItem("token", data.token);
-            // Redirection vers la page d'accueil si les informations d'identification sont correctes
-            window.location.href = 'index.html';
-            // Affichage du bouton de déconnexion et masquage du bouton de connexion
-        })
-        .catch(error => {
-            // Affichage d'un message d'erreur si les informations d'identification sont incorrectes ou si une erreur inattendue s'est produite
-            alert("Les informations d\'identification sont incorrectes");
-        });
-}
-
-
-// Ajout d'un événement d'écouteur pour le bouton de connexion
-const loginButton = document.querySelector('#login-button');
-loginButton.addEventListener('click', handleLogin);
